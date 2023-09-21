8536e1db95aadfa5890ea78661273c07a0e11b43
thuhoa0810 <thuhoa08102003@gmail.com>
2023-09-21 21:52:37 +0700

    initiate project frontend

---
+++ FrontEnd/demo.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        List<Interger> arr = Array.asList(1,2,4,8);
+        for (int i = 0; i < ar.size() - 1; i++){
+            System.out.println(arr.get(i));
+        }
+        
+    }
+}
